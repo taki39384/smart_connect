23e4003228780ce5d4b9bfb7bd7f7464f3be6809
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-11 17:57:13 +0900

    SNS認証でのログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,11 @@ class User < ApplicationRecord
       nickname: auth.info.name,
         email: auth.info.email
     )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 end
