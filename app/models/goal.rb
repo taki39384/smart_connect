8933d026e97f721365d4a399450574deb5c01560
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-17 10:49:44 +0900

    保存できない問題継続中

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,7 @@ class Goal < ApplicationRecord
   belongs_to :user
 
   validates :goal_name, presence: true
-  validates :goal_number, presence: true
-  validates :achievement_number, presence: true
+  validates :goal_number, presence: true, numericality: { only_integer: true }
+  validates :achievement_number, presence: true, numericality: { only_integer: true }
   
 end
