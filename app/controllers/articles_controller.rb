88d1ed89393c25e45b0a6ddd19fd2bd9785c2b05
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-20 19:43:42 +0900

    記事投稿機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,18 @@
 class ArticlesController < ApplicationController
   def new
+    @articles = Article.order("id DESC")
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      redirect_to new_article_url
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:text)
   end
 end
